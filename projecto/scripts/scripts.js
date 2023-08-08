487dd630c360ed130c475394a67a1588a849e1ad
ritasp <ritasalespelagio@gmail.com>
2023-08-08 16:53:15 +0100

    projeto - parte IV

--- projecto/scripts/scripts.js
+++ projecto/scripts/scripts.js
@@ -5,3 +5,20 @@ $(document).ready(function(){
         arrows: true,
       });
 });
+
+const questions = document.querySelectorAll('.faq-question');
+
+questions.forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        const arrow = question.querySelector('.arrow');
+
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+            arrow.style.transform = 'rotate(0deg)';
+        } else {
+            answer.style.display = 'block';
+            arrow.style.transform = 'rotate(180deg)';
+        }
+    });
+});
