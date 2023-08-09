34b2651f5860990a8706b85752df280a3f91cf1c
ritasp <ritasalespelagio@gmail.com>
2023-08-09 15:07:53 +0100

    projeto - parte V

--- projecto/enviar_mensagem.php
+++ projecto/enviar_mensagem.php
@@ -1,41 +1,42 @@
 <?php
-require 'mailer/PHPMailerAutoload.php';
-if (isset($_POST['nome']) && !empty($_POST['nome'])) {
-    $assunto = $_POST['nome'];
-}
-if (isset($_POST['email']) && !empty($_POST['email'])) {
-    $assunto = $_POST['email'];
-}
-if (isset($_POST['assunto']) && !empty($_POST['assunto'])) {
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+require 'PHPMailer/Exception.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
     $assunto = $_POST['assunto'];
-}
-if (isset($_POST['mensagem']) && !empty($_POST['mensagem'])) {
     $mensagem = $_POST['mensagem'];
-}
 
-$mail = new PHPMailer;
+    $mail = new PHPMailer();
 
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'tls';
-$mail->Username = 'exemplo@gmail.com';
-$mail->Password = 'senha';
-$mail->Port = 587;
+    // SMTP Configuration
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'corticodoguadiana@gmail.com';
+    $mail->Password = 'pass';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
 
-$mail->setFrom('email@gmail.com', 'Contato');
-$mail->addAddress('email@mail.com');
+    $mail->setFrom($email, $nome);
 
-$mail->isHTML(true);
+    $mail->addAddress('corticodoguadiana@gmail.com', 'Cortiço do Guadiana');
 
-$mail->Subject = $assunto;
-$mail->Body    = nl2br($mensagem);
-$mail->AltBody = nl2br(strip_tags($mensagem));
+    $mail->isHTML(true);
+    $mail->Subject = $assunto;
+    $mail->Body = 'Nome: ' . $nome . '<br>Email: ' . $email . '<br>Mensagem: ' . $mensagem;
 
-if(!$mail->send()) {
-    echo 'Não foi possível enviar a mensagem.';
-    echo 'Erro: ' . $mail->ErrorInfo;
-} else {
-    header('Location: index.php?enviado');
+    if ($mail->send()) {
+        echo 'Obrigado(a) pela sua mensagem!';
+    } else {
+        echo 'A sua mensagem não foi enviada. Por favor, tente novamente!';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    }
 }
 ?>
